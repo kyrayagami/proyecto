cb639b4974218c1559c165dc6e9f42dd94ce6b81
Adair Acevedo <kingdragon1@live.com.mx>
2016-01-26 22:19:24 -0500

    Migracion completas
    add table horarios

---
+++ database/migrations/2016_01_27_023151_add_horarios_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddHorariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::create('horarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('dia_id')->unsigned();
+            $table->integer('programa_id')->unsigned();
+            $table->time('hora_inicio');
+            $table->time('hora_termino');
+            $table->text('tipo');
+            $table->text('descripcion');
+            $table->text('imagen');
+            $table->foreign('dia_id')->references('id')->on('dias');
+            $table->foreign('programa_id')->references('id_programa')->on('programas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('horarios');
+    }
+}
