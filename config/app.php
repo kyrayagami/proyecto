0ac189e4bf654f8216bd4fc6bad2f621f98e5850
machan605 <machan605@gmail.com>
2016-01-27 00:33:40 -0500

    NavBar
    Faltan detalles

--- config/app.php
+++ config/app.php
@@ -156,6 +156,12 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+        *provider de laravelcollective html
+        */
+
+         Collective\Html\HtmlServiceProvider::class,
+         Cviebrock\EloquentSluggable\SluggableServiceProvider::class,
     ],
 
     /*
@@ -201,7 +207,11 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-
+        /*
+        *aliase externos de laravel collective
+        */
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
---
+++ resources/views/admin/template/main.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>@yield('title', 'Default')|Panel de administracion.</title>
+	<link rel="stylesheet" href="{{ asset('plugins/bootstrap/css/bootstrap.css')}}">
+	<link rel="stylesheet" href="{{asset('plugins/chosen/chosen.css')}}">
+	<link rel="stylesheet" href="{{asset('plugins/trumbowyg/ui/trumbowyg.css')}}">
+
+	<!--inicio externos-->
+	<link rel="stylesheet" href="{{asset('plugins/bootstrap_admin/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+	<link rel="stylesheet" href="{{asset('plugins/dist_admin/css/AdminLTE.min.css')}}">	 		
+	<link rel="stylesheet" href="{{asset('plugins/dist_admin/css/skins/_all-skins.min.css')}}">  
+	<link rel="stylesheet" href="{{asset('plugins/dist_admin/css/skins/_all-skins.min.css')}}">
+	<link rel="stylesheet" href="{{asset('plugins/plugins_admin/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
+	<link rel="stylesheet" href="{{asset('plugins/dist_admin/css/jquery-ui-1.10.3.custom/jquery-ui-1.10.3.custom/css/no-theme/jquery-ui-1.10.3.custom.min.css')}}">
+	<!--fin externos-->
+
+</head>
+<body>
+	@include('admin.template.partials.nav')
+
+	<section>
+		@yield('content')
+	</section>
+	
+
+	<script src="{{asset('plugins/jquery/js/jquery-2.1.4.js')}}"></script>
+	<script src="{{asset('plugins/bootstrap/js/bootstrap.js')}}" ></script>
+	<script src="{{asset('plugins/chosen/chosen.jquery.js')}}"></script>
+	<script src="{{asset('plugins/trumbowyg/trumbowyg.js')}} "></script>
+	
+	<!--inicio externos-->
+	<script src="{{asset('plugins/bootstrap_admin/js/bootstrap.min.js')}} "></script>
+	<script src="{{asset('plugins/plugins_admin/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js')}} "></script>
+	<script src="{{asset('plugins/plugins_admin/slimScroll/jquery.slimscroll.min.js')}} "></script>
+	<script src="{{asset('plugins/plugins_admin/fastclick/fastclick.js')}} "></script>
+	<script src="{{asset('plugins/dist_admin/js/app.min.js')}} "></script>
+	<!--fin externos-->
+	@yield('js')
+</body>
+</html>
