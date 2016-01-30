af779c4cf5041f92e723a1759294c6fb50986646
Adair Acevedo <kingdragon1@live.com.mx>
2016-01-30 16:15:53 -0500

    actualizacion de las migraciones

--- database/migrations/2016_01_26_174652_add_programas_table.php
+++ database/migrations/2016_01_26_174652_add_programas_table.php
@@ -12,7 +12,7 @@ class AddProgramasTable extends Migration
      */
     public function up()
     {
-        Schema::create('programas', function (Blueprint $table) {
+        Schema::create('programas', function (Blueprint $table) { 
             $table->increments('id');
             $table->string('nombre');            
             $table->string('estatus');
@@ -26,14 +26,18 @@ class AddProgramasTable extends Migration
             $table->integer('categoria_id')->unsigned();
             $table->integer('productor_id')->unsigned();            
 
-            $table->foreign('categoria_id')->references('id')->on('categorias');
-            $table->foreign('productor_id')->references('id')->on('productores');                        
+            $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('cascade');
+            $table->foreign('productor_id')->references('id')->on('productores')->onDelete('cascade');
             $table->timestamps();
         });
         Schema::create('conductor_programa', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('conductor_id')->unsigned();
             $table->integer('programa_id')->unsigned();
+
+            $table->foreign('conductor_id')->references('id')->on('conductores')->onDelete('cascade');
+            $table->foreign('programa_id')->references('id')->on('programas')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
@@ -45,6 +49,7 @@ class AddProgramasTable extends Migration
      */
     public function down()
     {
+        Schema::drop('conductor_programa');
         Schema::drop('programas');
     }
 }
