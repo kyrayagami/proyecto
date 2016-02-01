883965f3602ed3eb4755a19d0e21f752ada7ca0c
machan605 <machan605@gmail.com>
2016-02-01 12:03:32 -0500

    semana

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,12 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserTableSeeder::class);
-<<<<<<< HEAD
-        $this->call(ProductorSeeder::class);
-        //$this->call(ConductorSeeder::class);
-=======
+
         //$this->call(ProductorSeeder::class);
-        $this->call(conductor2::class);
->>>>>>> origin/master
+        //$this->call(conductor2::class);
+        //$this->call(DiasSemana::class);
     }
 }
