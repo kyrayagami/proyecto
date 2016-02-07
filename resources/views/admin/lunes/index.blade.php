f13153edb720d875834845e6720c06b304759876
machan605 <machan605@gmail.com>
2016-02-07 00:19:23 -0500

    vista

--- resources/views/admin/lunes/index.blade.php
+++ resources/views/admin/lunes/index.blade.php
@@ -4,14 +4,16 @@
 
 @section('content')  
 
-  <section class="content">    
+<section class="content">    
       <div class="row">
-          <div class="col-xs-3">                     
+        <div class="col-xs-3">                     
           <div class="box box-solid">
             <div class="box-header with-border">
               <h4 class="box-title">Semana</h4>
             </div>
             <div class="box-body">
+              <!-- the events -->
+
                   <div id="external-events">
                     <form method="post" action="" id="frm_dias" name="frm_dias" >
                       <button onClick=" window.location.href='{{route('admin.domingo.index')}}' " type="button" class="btn btn-block bg-yellow btn-default">Domingo</button>
@@ -20,10 +22,12 @@
                       <button onClick=" window.location.href='{{route('admin.miercoles.index')}}' " type="button" class="btn btn-block bg-light-blue btn-default">Miercoles</button>
                       <button onClick=" window.location.href='{{route('admin.jueves.index')}}' " type="button" class="btn btn-block bg-red btn-default">Jueves</button>
                       <button onClick=" window.location.href='{{route('admin.viernes.index')}}' " type="button" class="btn btn-block bg-purple btn-default">Viernes</button>
-                      <button onClick=" window.location.href='{{route('admin.sabado.index')}}' " type="button" class="btn btn-block bg-orange btn-default">Sabado</button>    
+                      <button onClick=" window.location.href='{{route('admin.sabado.index')}}' " type="button" class="btn btn-block bg-orange btn-default">Sabado</button> 
                     </form>                                                      
                   </div>
+ 
             </div>
+            <!-- /.box-body -->
           </div> 
         </div>
         <div class="col-xs-9"> 
@@ -39,26 +43,27 @@
                   <tr>
                   <td align="left" valign="top" width="670">
                     <div id="page-wrap" align="center"> 
-                      <div id="example-one" align="center"> 
-
-                       
+                      <div id="example-one" align="center">              
                         <ul class="nav">
+
                           <li class="espacio-nav">&nbsp;</li>
                           <li class="nav-one"><a href="#manana">MAÑANA</a></li>
                           <li class="nav-two"><a href="#tarde" class="current" >TARDE</a></li>
-                          <li class="nav-three"><a href="#noche">NOCHE</a></lu>                                         
-                        </ul> 
-                        <?php $acu=0; ?>            
+                          <li class="nav-three"><a href="#noche">NOCHE</a></li>                                                               
+                        </ul>  
+
+                        <?php $acu=0; ?>
+
                         <div id="prueba" align="center">      
-                          <div class="list-wrap" align="center"> 
+                          <div class="list-wrap" align="center">                            
                             <ul style="position: relative; top: 0px; left: 0px; display: none;" id="manana" >
                               @foreach($L as $lu)
                               <?php $acu = $acu+1; $la="layer".$acu; $muestra= "show('layer".$acu."')";?>
                               <?php $hora=str_replace(":", "", $lu->hora_inicio)?>
-                              @if($hora<115959)
+                              @if($hora<115959)  
                               <li class="estilo_lista" onclick="{{$muestra}}"> 
                                 <b>{{$lu->hora_inicio}}</b> &nbsp;&nbsp;&nbsp;{{$lu->programa->nombre}}
-                                <div id="{{$la}}" style="display: block;" class="layer"> 
+                                <div id="{{$la}}" style="display: none;" class="layer"> 
                                 <img src="{{asset('plugins/dist_admin/img/photo3.jpg')}}" hspace="0" vspace="0" width="200" height="133" border="0">
                                 <img src="{{asset('plugins/dist_admin/img/flecha-left.png')}}" hspace="0" vspace="0" width="10" height="133" border="0">
                                   <div class="sub-layer"> 
@@ -73,18 +78,16 @@
                                     <p></p>
                                   </div> 
                                 </div>
-                              </li> 
-                                 
+                              </li>
                               @endif
-                              @endforeach     
-                            </ul> 
-                            <ul id="tarde">  
-
-                             @foreach($L as $lu)
+                              @endforeach                     
+                            </ul>                        
+                            <!--- tarde -->
+                            <ul id="tarde">
+                              @foreach($L as $lu)
                               <?php $acu = $acu+1; $la="layer".$acu; $muestra= "show('layer".$acu."')";?>
                               <?php $hora=str_replace(":", "", $lu->hora_inicio)?>
-                              @if($hora>=120000 && $hora<=185959)
-
+                              @if($hora>=120000 && $hora<=1859599)  
                               <li class="estilo_lista" onclick="{{$muestra}}"> 
                                 <b>{{$lu->hora_inicio}}</b> &nbsp;&nbsp;&nbsp;{{$lu->programa->nombre}}
                                 <div id="{{$la}}" style="display: none;" class="layer"> 
@@ -101,19 +104,18 @@
                                     </p>
                                     <p></p>
                                   </div> 
-                                  </div>
-                              </li>  
-
+                                </div>
+                              </li>
                               @endif
-                              @endforeach        
+                              @endforeach 
                             </ul>  
-                          <ul style="position: relative; top: 0px; left: 0px; display: none;" id="noche" >
-                            @foreach($L as $lu)
+                            <!--- noche -->     
+                            <ul style="position: relative; top: 0px; left: 0px; display: none;" id="noche" >
+                              @foreach($L as $lu)
                               <?php $acu = $acu+1; $la="layer".$acu; $muestra= "show('layer".$acu."')";?>
                               <?php $hora=str_replace(":", "", $lu->hora_inicio)?>
-                              @if($hora>=190000 && $hora<=235959)
-
-                            <li class="estilo_lista" onclick="{{$muestra}}">
+                              @if($hora>=190000 && $hora<=235959)  
+                              <li class="estilo_lista" onclick="{{$muestra}}"> 
                                 <b>{{$lu->hora_inicio}}</b> &nbsp;&nbsp;&nbsp;{{$lu->programa->nombre}}
                                 <div id="{{$la}}" style="display: none;" class="layer"> 
                                 <img src="{{asset('plugins/dist_admin/img/photo3.jpg')}}" hspace="0" vspace="0" width="200" height="133" border="0">
@@ -129,31 +131,31 @@
                                     </p>
                                     <p></p>
                                   </div> 
-                                  </div>
-                              </li>  
+                                </div>
+                              </li>
                               @endif
-                              @endforeach   
-       
-                            </ul> 
-                          </div>
-                        </div>
-                        </div>
-                      </div>
-                    </td>
+                              @endforeach
+                              <!---->
+                            </ul>
+                          </div> <!-- END List Wrap -->    
+                        </div> <!-- prueba -->
+                      </div> <!-- END Organic Tabs (Example One) -->                                  
+                    </div>
+                  </td>
                   </tr>
-                </tbody>
-              </table>
-          </div>
+                </tbody>              
+              </table>                
+            </div>                    
+          <!-- box -->
+        </div>
       </div>
-   </div>
-  </div>
 </section>
 
 @endsection
 
 @section('js')
 
-    <script>
+    <script type="text/javascript">
         $(function() {
     
             $("#example-one").organicTabs();
@@ -164,7 +166,7 @@
     
         });
     </script>
-  <script>
+  <script type="text/javascript">
     function show(bloq) {
       obj = document.getElementById(bloq);
      obj.style.display = (obj.style.display=='none') ? 'block' : 'none';
