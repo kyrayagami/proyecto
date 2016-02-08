811c89bcc88d963e92d00b95afad9aece781f8a0
Adair Acevedo <kingdragon1@live.com.mx>
2016-02-08 13:33:29 -0500

    casi listo
    imprimir horario

--- app/Http/Controllers/HorarioPDFController.php
+++ app/Http/Controllers/HorarioPDFController.php
@@ -15,69 +15,7 @@ use App\Programa;
 class HorarioPDFController extends Controller
 {
     public function index(){
-    	$horarios= Horario::orderBy('dia_id','ASC')->orderBy('hora_inicio','ASC')->get();
-        $dia=1;
-        $i=0;
-        $j=0;
-        $mayor=0;
-        $pos=0;
-        $cont=0;
-        $array1='';
-        $entro=0;
-        foreach ($horarios as $horario) {
-            if($horario->dia_id!=$dia){
-                $dia=$horario->dia_id;
-                $i=0;
-                $j++;
-                $cont=0;
-                //$aux=$cont+100000;
-            }
-            //$lim=235959;            
-            $aux=$cont+10000;
-            while($entro==0){
-                $hora=str_replace(":", "",$horario->hora_inicio);
-                if($hora>=$cont && $hora<$aux){
-                    $array1[$i][$j]='<td>'
-                        .$horario->dia_id.'&nbsp'.$horario->programa->nombre.
-                        '<br>'.$horario->hora_inicio.
-                        '<br>'.$horario->hora_termino.
-                        '<br>'.$cont.
-                        '</td>';
-                    $cont=$cont+10000;
-                    $entro=1;
-                }else{
-                    $array1[$i][$j]='<td>vacio</td>';
-                    $cont=$cont+10000;
-                    $aux=$cont+10000;                    
-                }                
-                $i++;
-            }
-            $entro=0;            
-            //$i++;
-            if($i>$mayor)
-                $mayor=$i;
-        }            
-        $aux=10;
-        $array2='';
-        for ($i=0; $i < $mayor; $i++) { 
-            $array2[$i]='<tr>';
-            for ($j=0; $j < 2; $j++) {          
-                /*
-                if(empty($array1[$i][$j]))
-                {
-                    $array2[$i].='<td></td>';
-                }
-                else{
-                    $array2[$i].=$array1[$i][$j];
-                }*/
-                $array2[$i].=$array1[$i][$j];
-            /*if(count($array1[$i])>$aux)
-                $mayor=$array1[$i][$j];
-            */
-            }
-            $array2[$i].='</tr>';       
-        }
-    //return $array2;
-    	return view('admin.horario_pdf.index')->with('contenido',$array2);
+        $horarios= Horario::orderBy('hora_inicio','ASC')->orderBy('dia_id','ASC')->get();
+        return view('admin.horario_pdf.index')->with('horarios',$horarios);
     }
 }
--- resources/views/admin/horario_pdf/index.blade.php
+++ resources/views/admin/horario_pdf/index.blade.php
@@ -17,24 +17,59 @@
         <table id="thorario" class="table table-condensed">          
           <thead>
             <tr>
-               <!-- <th style="width: 10px">Horario</th>-->
+              <th style="width: 10px">Horario</th>
               <th>Lunes</th>                    
-              <th>Martes</th>    
-              <!--                  
+              <th>Martes</th>                  
               <th>Miercoles</th>
               <th>Jueves</th>
               <th>Viernes</th>
               <th>Sabado</th>
-              <th>Domingo</th>-->
-            </tr>  
+              <th>Domingo</th>
+            </tr> 
           </thead>              
-          <tbody id="lis_horario">
-            <?php                
-              $conta=0;                
-              while(count($contenido)>$conta){
-                echo $contenido[$conta];
-                $conta++;
-              }
+          <tbody id="lis_horario">          
+            <?php                   
+            $inicio=0;
+            $termino=$inicio+10000;
+            $dia=1;
+            echo "<tr> <td>".$inicio."</td>";
+            // un while para que imprima todas las horas
+              foreach ($horarios as $horario){                
+                $h_ini=str_replace(":","",$horario->hora_inicio);
+                $h_end=str_replace(":","",$horario->hora_termino);                
+                if($h_ini>=$termino){  
+                  $inicio=$inicio+10000;
+                  $termino=$inicio+10000;
+                  while($dia<=7){
+                    echo "<td>vacio</td>";
+                    $dia++;
+                  }
+                  echo "</tr>";
+                  //empieza una nueva hora
+                  $dia=1;
+                  echo "<tr><td>".$inicio."</td>";
+                }
+
+                if($h_ini>=$inicio && $h_end<=$termino){
+                  //echo '<tr>';
+                  while($horario->dia_id>=$dia){
+                    if($dia==$horario->dia_id){
+                      echo "<td>".$horario->programa->nombre. ' -> '.$horario->hora_inicio."</td>";
+                      $dia++;
+                      break;
+                    }
+                    else{ 
+                      echo "<td>vacio</td>";
+                      $dia++;
+                    }
+                  }
+                }
+                else{
+                  //echo "<td>vacio</td> <td>vacio</td> <td>vacio</td> <td>vacio</td> <td>vacio</td> <td>vacio</td> <td>vacio</td> </tr>";
+                  echo "<td>Todo vacio </td></tr>";
+                  $dia=8;
+                }                
+              }         
             ?>
           </tbody>
         </table>
